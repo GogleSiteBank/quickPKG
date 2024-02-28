071613e3abdf9b93b67043b9bdb9ab088f864ac9
sPYracy <125816677+GogleSiteBank@users.noreply.github.com>
2024-02-28 21:24:30 +0000

    + Better File Names + File Name in description + Download bar
    Filenames now wont be the url strong but instead the game name
    Descriptions now contain the File Name, e.g:
      DDL: http://zeus.dl.playstation.net/cdn/JP9000/NPJG00004_00/qnJkc6fUfavnVQYg3w4Fc97vqf6VV4gWTV5tGrD4XW0Qf6x80Cb9ItsBIpiu6VN6qfD5AsJdV7haI9NfjRDajSsO4S0nMq5DDpJyw.pkg
      Name: NBA 2K12
      File Name: NBA 2K12.pkg
    Download/Progress bar with filesize, remaining time, speed

--- downloader.py
+++ downloader.py
@@ -1,4 +1,5 @@
-import time, requests
+import time, requests, re
+from tqdm import tqdm
 start_time = time.time()
 
 ddls = open("ddls.txt", "r").readlines()
@@ -14,15 +15,28 @@ try:
 except FileNotFoundError:
     print("titles.txt not found! Make sure to use parser.py before downloading.")
 
+filereadable = lambda s: re.sub(r'[^\w\s]', '', s.strip().replace('[', '').replace(']', ''))[:255]
+
+
 try:
     getDDL = int(input("\nGame Number: "))
-    print("Downloading game:\nDDL: %s\nName: %s" % (ddls[getDDL-1].strip(), names[getDDL-1]))
-    content = requests.get(ddls[getDDL-1].strip()).content
-    print("Got game file, writing file...")
-    pkg = (ddls[getDDL-1].strip())[::-1][:20][::-1]
+    print("Downloading game:\nDDL: %s\nName: %s\nFile Name: %s" % (ddls[getDDL-1].strip(), names[getDDL-1], filereadable(names[getDDL-1]) + ".pkg"))
 
-    open(pkg, "wb").write(content)
-    print("File Written! (%s)" % pkg)
+    url = ddls[getDDL-1].strip()
+    response = requests.get(url, stream=True)
+    total_size = int(response.headers.get('content-length', 0))
+
+    with open(filereadable(names[getDDL-1]) + ".pkg", 'wb') as file, tqdm(
+        desc="Downloading",
+        total=total_size,
+        unit='B',
+        unit_scale=True,
+        unit_divisor=1024,
+        ) as progress_bar:
+        for data in response.iter_content(chunk_size=1024):
+            file.write(data)
+            progress_bar.update(len(data))
+
 except ValueError:
     print("ValueError - Incorrect Number")
 except IndexError:
